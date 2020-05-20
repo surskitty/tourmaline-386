bcc1841ad8be03c0a77bcd5e8b5842d1cce4efb9
surskitty <surskitty@gmail.com>
2020-05-19 23:33:11 -0400

    Steven ought to help you with your eevee :)

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -87,8 +87,8 @@
 #define FLAG_UNUSED_0x04B    0x4B // Unused Flag
 #define FLAG_UNUSED_0x04C    0x4C // Unused Flag
 #define FLAG_UNUSED_0x04D    0x4D // Unused Flag
-#define FLAG_UNUSED_0x04E    0x4E // Unused Flag
-#define FLAG_RECEIVED_EEVEE  0x4F // Unused Flag
+#define FLAG_RECEIVED_STONES 0x4E // Evolution Stones from Steven
+#define FLAG_RECEIVED_EEVEE  0x4F // Starter Eevee from rival
 
 // Scripts
 #define FLAG_HIDE_RAYQUAZA_SKY_TOWER_SUMMIT  0x50
