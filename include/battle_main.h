00ba9d9cbbea2c8ef1e5bba88288a0466da30175
surskitty <surskitty@gmail.com>
2020-10-17 22:52:59 -0400

    Making random trainers use different pokeball types.

--- include/battle_main.h
+++ include/battle_main.h
@@ -7,6 +7,12 @@ struct TrainerMoney
     u8 value;
 };
 
+struct TrainerBall
+{
+    u8 classId;
+    u8 Ball; // make this a u16 if needed
+};
+
 struct UnknownPokemonStruct4
 {
     /*0x00*/ u16 species;
